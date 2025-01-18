a367923de6f3986701aeeb997fea434b1f0a9ee1
Matt Pahuta <mhpahuta@gmail.com>
2025-01-18 12:45:17 -0800

    Tidy styles, js, and markup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Character sets for password generation
+// symbols list from OWASP (https://owasp.org/www-community/password-special-characters)
 const CHAR_SETS = {
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowercase: "abcdefghijklmnopqrstuvwxyz",
@@ -7,59 +8,33 @@ const CHAR_SETS = {
 };
 // Global variables
 const passwordOutput = document.getElementById("passwordOutput");
-const copyBtn = document.getElementById('copyBtn');
-const strengthBars = document.querySelectorAll('.bar');
-const strengthRating = document.getElementById('strengthRating');
+const copyBtn = document.getElementById("copyBtn");
+const strengthBars = document.querySelectorAll(".bar");
+const strengthRating = document.getElementById("strengthRating");
+const alertDialog = document.getElementById("alert");
+const alertCloseBtn = document.getElementById("alert-close-btn");
 
-const alertDialog = document.getElementById('alert');
-const alertCloseBtn = document.getElementById('alert-close-btn');
-
-// symbols list from OWASP (https://owasp.org/www-community/password-special-characters)
-
-// Get character length value from input range slider
-// - values from 1 to 20
-// Get checked values from character type selection checkboxes
-// - uppercase, lowercase, numbers, symbols
-// - must make minimum of 1 selection
-// Listen for 'submit' events on button
-// - collect form data
-// - call generatePassword function
-// - clear any existing displayed passwords from output field
-// - call render password function
-
-// *** Display Logic ***
-// Render generated password in output field
-// - clear placeholder value
-// - update opacity to 1
-// Listen for clicks on 'copy' button, allow for copying to clipboard of generated pw
-// Render strength rating in Strength field
-// - update label to correct value (too weak!, weak, medium, strong)
-// - update strength strengthBars to render corresponding color coding
-
-// Generate password from selected length and character options
+// Generate password
 function generatePassword(length, options) {
-  let allChars = '';
-  let password = '';
-  // Add selected pw attribute options
+  let allChars = "";
+  let password = "";
   if (options.uppercase) allChars += CHAR_SETS.uppercase;
   if (options.lowercase) allChars += CHAR_SETS.lowercase;
   if (options.numbers) allChars += CHAR_SETS.numbers;
   if (options.symbols) allChars += CHAR_SETS.symbols;
-  // Generate random password - select chars from allChars pool
+
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * allChars.length);
     password += allChars[randomIndex];
   }
-  console.log('AllChars: ', allChars)
   return password;
 }
 
 // Display generated password
 function renderPassword(password) {
-  passwordOutput.textContent = password
+  passwordOutput.textContent = password;
   passwordOutput.style.opacity = 1;
   passwordOutput.dataset.copy = "ready";
-  console.log(passwordOutput.dataset.copy) // debug
 }
 
 // Evaluate password strength
@@ -67,89 +42,64 @@ function evaluatePasswordStrength(password, options) {
   let score = 0;
   if (password.length >= 8) score += 1;
   if (password.length >= 12) score += 1;
-  if (options.uppercase && /[A-Z]/.test(password)) score += 1; // Uppercase check
-  if (options.lowercase && /[a-z]/.test(password)) score += 1; // Lowercase check
-  if (options.numbers && /[0-9]/.test(password)) score += 1;   // Number check
-  if (options.symbols && /[!#$%& '()*+,-./:;<=>?@[\]^_`{|}~]/.test(password)) score += 1; // Symbol check
-  // debug
-  console.log('Score: ', score)
+  if (options.uppercase && /[A-Z]/.test(password)) score += 1;
+  if (options.lowercase && /[a-z]/.test(password)) score += 1;
+  if (options.numbers && /[0-9]/.test(password)) score += 1;
+  if (options.symbols && /[!#$%& '()*+,-./:;<=>?@[\]^_`{|}~]/.test(password)) score += 1;
   renderStrengthOutput(score)
 }
 
-// Render password strength label and indicator
-// ToDo: rename to renderStrengthOutput
+// Render password strength label and indicator bars
 function renderStrengthOutput(ratingScore) {
-  // 0 - 2 = too weak - 1 bar, red
-  // 3 = weak - 2 strengthBars, orange
-  // 4 = medium - 3 strengthBars, yellow
-  // >= 5 = strong - 4 strengthBars, neon-green
-
-  /*
-    Requirements:
-    - render strength label (too weak, weak, etc.)
-    - match label with num of strengthBars to fill
-    - fill bar(s) with proper color
-
-    Use object to hold strength props?
-  */
-  resetStrengthOutput(); // clear any previous styles/content
-  
-  // Initialize an object to serve as a styles legend
+  resetStrengthOutput();
   const legend = {};
-  // use a switch statement to populate legend based on pw stength score
   switch (ratingScore) {
     case 0:
     case 1:
     case 2:
-      legend.ratingLabel = 'Too Weak!';
+      legend.ratingLabel = "Too Weak!";
       legend.barCount = 1;
       legend.barColor = 'hsl(0, 91%, 63%)';
       break;
     case 3:
-      legend.ratingLabel = 'Weak';
+      legend.ratingLabel = "Weak";
       legend.barCount = 2;
       legend.barColor = 'hsl(13, 95%, 66%)';
       break;
     case 4:
-      legend.ratingLabel = 'Medium';
+      legend.ratingLabel = "Medium";
       legend.barCount = 3;
       legend.barColor = 'hsl(42, 91%, 68%)';
       break;
     default:
-      legend.ratingLabel = 'Strong';
+      legend.ratingLabel = "Strong";
       legend.barCount = 4;
       legend.barColor = 'hsl(127, 100%, 82%)';
   }
 
-  console.log(legend); // debug
-  // create a proper array from the stengthBars node list
   const styledBars = Array.from(strengthBars).slice(0, legend.barCount);
-  strengthRating.textContent = legend.ratingLabel; // apply strength rating label
-  styledBars.forEach(bar => { // style corresponding strength bars
+  strengthRating.textContent = legend.ratingLabel;
+  styledBars.forEach(bar => {
     bar.style.backgroundColor = legend.barColor;
     bar.style.borderColor = legend.barColor;
-  })
-
+  });
 }
 
 // Reset strength label and indicator bars
 function resetStrengthOutput() {
-  strengthRating.textContent = '';
+  strengthRating.textContent = "";
   strengthBars.forEach(bar => { 
     bar.style.backgroundColor = 'transparent';
     bar.style.borderColor = 'hsl(252, 11%, 91%)';
-  })
+  });
 }
 
 // Handle password form submit 
 function handleFormSubmit(event) {
   event.preventDefault();
-
   const form = document.getElementById("passwordForm");
   const formData = new FormData(form);
-  // selected length value
   const length = parseInt(formData.get("length"));
-  // build options object from form data
   const options = {
     uppercase: formData.has("uppercase"),
     lowercase: formData.has("lowercase"),
@@ -157,68 +107,46 @@ function handleFormSubmit(event) {
     symbols: formData.has("symbols")
   };
 
-  // if all options are false (none selected), exit function
   if (!options.uppercase && !options.lowercase && !options.numbers && !options.symbols) {
-    // alert('select at least one option')
     alertDialog.showModal();
-    passwordOutput.dataset.copy === "idle"
-    console.log('updating pw output data attribute...')
     return;
   }
-  // debug
-  console.log('selected length: ', length)
-  console.log(options);
-  // generate password
   const password = generatePassword(length, options);
-  console.log(password) // debug
-  // console.log(evaluatePasswordStrength(password, options))
   evaluatePasswordStrength(password, options);
-  renderPassword(password); // moved to evaluatePasswordStrength
-
-  // ToDo: set passwordOutput data-copy to 'idle'
+  renderPassword(password);
 }
 
-// Copy PW to clipboard
+// Handle Password copy to clipboard
 async function handlePasswordCopy() {
   const password = passwordOutput.textContent;
   const copyMessage = document.getElementById("copyMessage");
 
   if (passwordOutput.dataset.copy === "ready") {
-    console.log('Copying password!')
     copyMessage.textContent = "copied";
-    await navigator.clipboard.writeText(password); // copy pw to clipboard
-    // Initial copy pw notification
-    // Fade out text after 1 second
+    await navigator.clipboard.writeText(password);
     setTimeout(() => {
-      copyMessage.style.transition = 'all 1s';
+      copyMessage.style.transition = "all 1s";
       copyMessage.style.opacity = 0;
-      // Remove styles and text after fade out
       setTimeout(() => {
-        copyMessage.style.removeProperty('opacity');
-        copyMessage.style.removeProperty('transition');
-        copyMessage.textContent = '';
+        copyMessage.style.removeProperty("opacity");
+        copyMessage.style.removeProperty("transition");
+        copyMessage.textContent = "";
       }, 1000);
     }, 1000);
   }
-
 }
-
-copyBtn.addEventListener('click', handlePasswordCopy);
-alertCloseBtn.addEventListener('click', () => {
-  alertDialog.close();
-});
 
 // Initialize event listeners
 document.addEventListener("DOMContentLoaded", () => {
   const passwordForm = document.getElementById("passwordForm");
-  // Handle and update length value selection
   const lengthInput = document.getElementById('lengthInput');
   const lengthValueOutput = document.getElementById('lengthOutput');
-  // Update selected password length display
   lengthInput.addEventListener("input", (event) => {
     lengthValueOutput.textContent = event.target.value;
   });
-
   passwordForm.addEventListener("submit", handleFormSubmit);
-
+  copyBtn.addEventListener('click', handlePasswordCopy);
+  alertCloseBtn.addEventListener('click', () => {
+    alertDialog.close();
+  });
 });
